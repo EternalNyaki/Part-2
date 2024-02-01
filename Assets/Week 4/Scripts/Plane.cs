47bee268925f56ce3a4095f52b71677d2d302a05
MSI\etern <reyjoshu@sheridancollege.ca>
2024-02-01 10:26:25 -0500

    Task 2: Made plane follow line (also forgot to save my scene in the last commit oopsies)

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
+    public float speed;
     public float newPositionThreshold = 0.2f;
+    public float pointArrivalThreshold = 0.05f;
+    public Vector2 currentPosition;
 
     public List<Vector2> points;
 
     private Vector2 lastPosition;
 
+    private Rigidbody2D rb2d;
     private LineRenderer lineRenderer;
 
     private void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if(points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < pointArrivalThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 1; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb2d.rotation = -angle;
+        }
+        rb2d.MovePosition(currentPosition + (Vector2)transform.up * speed * Time.deltaTime);
     }
 
     private void OnMouseDown()
