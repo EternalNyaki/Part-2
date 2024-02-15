5da6d52d870c1e97e53d278d5873e91c14e141a5
MSI\etern <reyjoshu@sheridancollege.ca>
2024-02-15 14:27:20 -0800

    Task 2: Created Scene B and made pressing space switch scenes

--- Assets/Week 6/Scripts/NextScene.cs
+++ Assets/Week 6/Scripts/NextScene.cs
@@ -7,6 +7,14 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+        }
+    }
+
     public void loadNextScene()
     {
         SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
