2cf6dece44d1908b5aa1951a2c26b8597e6e9bfb
MSI\etern <reyjoshu@sheridancollege.ca>
2024-02-22 11:19:07 -0500

    Task 2b: Can flick selected player using the spacebar

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
+    public float maxCharge = 1f;
+
+    public Slider chargeSlider;
+    
     public static Footballer selectedPlayer { get; private set; }
+
+    private float charge = 0;
+    private Vector2 direction;
 
     public static void SetSelectedPlayer (Footballer player)
     {
@@ -15,5 +23,36 @@ public class Controller : MonoBehaviour
 
         selectedPlayer = player;
         selectedPlayer.Select(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            selectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+
+    private void Update()
+    {
+        if (selectedPlayer == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            charge = Mathf.Clamp(charge + Time.deltaTime, 0, maxCharge);
+            chargeSlider.value = charge;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedPlayer.transform.position).normalized * charge;
+        }
     }
 }
--- Assets/Week 7/Scripts/Footballer.cs
+++ Assets/Week 7/Scripts/Footballer.cs
@@ -7,12 +7,17 @@ public class Footballer : MonoBehaviour
     public Color unselectedColour = Color.red;
     public Color selectedColour = Color.green;
 
+    public float speed = 100f;
+
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
+
         Select(false);
     }
 
@@ -30,5 +35,10 @@ public class Footballer : MonoBehaviour
     public void Select(bool selected)
     {
         spriteRenderer.color = selected ? selectedColour : unselectedColour;
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb2d.AddForce(direction * speed);
     }
 }
